c373119bfe69dec005255d70c0ad764f558a04fe
Ben Wiz <bwisialowski@gmail.com>
2019-02-26 10:14:27 -0500

    spotify auth implicit grant

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import Cookies from 'universal-cookie';
-import * as Spotify from 'spotify-web-api-js';
+import Spotify from 'spotify-web-api-js';
+import Util from './util';
 import './App.css';
 
-// Set up cookie manager (TODO: Put this block in a proper place)
-const cookies = new Cookies();
-
-// Set up Spotify (TODO: Put this block in a proper place)
+// Set up Spotify
 const spotify = new Spotify();
-
-// http://localhost:3000/?code=AQCxX3OSP8zojzDz0O3Gl4rdKGTS-icK3yF-EcBQNYKdRLlV12b8aeonTZo3CPpr6X2_dM8Hyp5x7C_cLus6WNFbVgyyNZ1PS7XB5IzZOIP9r4K_wxIb2nReuEgjqn7mFufJrVql9vNGmJcY1uRuf5_SM54RkP8tqETJ1DQ0Lp0kDaQmCYSD-7AtVp1-Xo7W1B8MEVoOOwJJNjnLc4SJXtvXpBPw6Af57--4NBw0z0PtoAL0xb3jlPIr9YGbgcjaSYRZ
-// TODO: If query param `code` exists, then use that as spotify access token (and save to cookie), else try cookie, else redirect
-
-let spotifyAccessToken = cookies.get('spotify-access-token');
-if (spotifyAccessToken) {
-  spotify.setAccessToken(spotifyAccessToken);
-} else {
-  console.log('redirect to get spotify access token');
-
-  const client_id = 'ff53948d58f1491baa6169d34bc4179a';
-  const scopes = encodeURIComponent(
-    'user-read-playback-state user-modify-playback-state'
-  );
-  const redirect_uri = window.location.origin;
-  const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${scopes}&redirect_uri=${redirect_uri}`;
-  window.location = url;
-}
+Util.setupSpotify(spotify);
 
 const StartButton = props => <button onClick={props.onClick}>Start</button>;
 
---
+++ src/util.js
@@ -0,0 +1,33 @@
+import Cookies from 'universal-cookie';
+import QueryString from 'querystring';
+
+const setupSpotify = spotify => {
+  const cookies = new Cookies();
+
+  // Get access token from Spotify implicit grant flow redirect callback, and set cookie
+  if (window.location.hash.length > 1) {
+    const querystring = window.location.hash.slice(1);
+    const query = QueryString.parse(querystring);
+    if (query.access_token) {
+      cookies.set('spotify-access-token', query.access_token);
+      // Final redirect to remove the hash, not really necessary, but url looks nicer
+      window.location = window.location.href.split('#')[0];
+    }
+  }
+
+  // Get token from cookie and use it to set up Spotify client, if not start the implicit grant flow
+  const spotifyAccessToken = cookies.get('spotify-access-token');
+  if (spotifyAccessToken) {
+    spotify.setAccessToken(spotifyAccessToken);
+  } else {
+    const client_id = 'ff53948d58f1491baa6169d34bc4179a';
+    const scopes = encodeURIComponent(
+      'user-read-playback-state user-modify-playback-state'
+    );
+    const redirect_uri = window.location.origin;
+    const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=${scopes}&redirect_uri=${redirect_uri}`;
+    window.location = url;
+  }
+};
+
+export default { setupSpotify };
