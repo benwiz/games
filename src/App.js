6612a4721c629798b6dd16753b911d48e86a10a1
Ben Wiz <bwisialowski@gmail.com>
2019-02-25 21:38:08 -0500

    change handler for dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import * as Spotify from 'spotify-web-api-js';
 import './App.css';
+
+const spotify = new Spotify();
 
 const StartButton = props => <button onClick={props.onClick}>Start</button>;
 
+const DeviceSelect = props => (
+  <select value={props.value} onChange={props.onChange}>
+    <option value="12sx">Phone</option>
+    <option value="8e56">Mac</option>
+  </select>
+);
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      device: '', // TODO: First device should be selected
+    };
+  }
+
+  deviceSelectChangeHandler(event) {
+    console.log('change event:', event);
+  }
+
   startButtonClickHandler() {
     console.log('startButtonClickHandler()');
   }
@@ -11,6 +32,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <DeviceSelect
+          value={this.state.device}
+          onChange={this.deviceSelectChangeHandler}
+        />
         <StartButton onClick={this.startButtonClickHandler} />
       </div>
     );
