f652514445375c198cfcef15a2791ff134b3ed62
Ben Wiz <bwisialowski@gmail.com>
2019-02-25 22:22:47 -0500

    load devices from spotify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-// import * as Spotify from 'spotify-web-api-js';
+import * as Spotify from 'spotify-web-api-js';
 import './App.css';
 
-// const spotify = new Spotify();
+// Set up Spotify (TODO: Put this in a proper place)
+const spotify = new Spotify();
+spotify.setAccessToken(
+  'BQCI2wZ-GE4k35VQY4wrqs-HVnm2n4eWI93WqayyMZgR_8BSOXeiJnI8XnvVdvTHgZRXWg5GAqoHTST1eTOlnhLm7lmTQRjD_zPYE-WMrR_UVrg98sJOBHID2jGvhver9KgF00V0k492S4eTdcc-kpcJxsriustJGltn3p_v8IZ_1j8HKCo6lZQqPjBmPoyIPzaSIwrlY48dS3JnS5gnuU71'
+);
 
 const StartButton = props => <button onClick={props.onClick}>Start</button>;
 
@@ -11,8 +15,8 @@ class DeviceSelect extends React.Component {
     const options = [];
     for (const device of this.props.devices) {
       const option = (
-        <option key={device} value={device}>
-          {device}
+        <option key={device.id} value={device.id}>
+          {device.id}
         </option>
       );
       options.push(option);
@@ -33,21 +37,25 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    // Get devices from spotify
-    const devices = ['phone', 'computer'];
-
     // Set initial state
     this.state = {
-      devices,
-      device: devices[0],
+      devices: [],
+      currentDeviceID: '',
     };
 
     // Bind class functions
     this.deviceSelectChangeHandler = this.deviceSelectChangeHandler.bind(this);
   }
 
+  async componentDidMount() {
+    // Load data from Spotify
+    const { devices } = await spotify.getMyDevices();
+    this.setState({ devices, device: devices[0].id });
+  }
+
   deviceSelectChangeHandler(event) {
-    this.setState({ device: event.target.value });
+    console.log('change');
+    // this.setState({ device: event.target.value });
   }
 
   startButtonClickHandler() {
@@ -58,7 +66,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <DeviceSelect
-          value={this.state.device}
+          value={this.state.currentDeviceID}
           onChange={this.deviceSelectChangeHandler}
           devices={this.state.devices}
         />
