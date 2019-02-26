60fab2a8465ab2149c31920f270850ac96aa1f98
Ben Wiz <bwisialowski@gmail.com>
2019-02-25 21:25:45 -0500

    build with button

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48e3f2479149bb28c053b39f7721cd26.js"
+  "/precache-manifest.615eef304b8fd023dea305e2e64f99d3.js"
 );
 
 workbox.clientsClaim();
