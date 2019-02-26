62360ae9fd27f5cb5bbb7b7278c419916e300496
Ben Wiz <bwisialowski@gmail.com>
2019-02-25 21:22:03 -0500

    center button

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # power-hour
+
+A controller to run a power hour using Spotify.
+
+## Version 1 Plan
+
+### How to use
+
+1. Start music on Spotify like normal (possibly within desired playlist)
+2. Click the "start" button. Songs will begin changing every 60 seconds.
+
+## To Do
+
+- "Start" button component
+- Spotify login
+- Component speaks with Spotify
--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
 
+const StartButton = props => <button onClick={props.onClick}>Start</button>;
+
 class App extends React.Component {
+  startButtonClickHandler() {
+    console.log('startButtonClickHandler()');
+  }
+
   render() {
-    return <div className="App">hey</div>;
+    return (
+      <div className="App">
+        <StartButton onClick={this.startButtonClickHandler} />
+      </div>
+    );
   }
 }
 
