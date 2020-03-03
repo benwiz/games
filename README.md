91322daa8141b5c2ed2bc8a0046d5250d883a7d4
Ben Wiz <benwiz@Bens-MacBook-Air.local>
2020-03-03 16:43:23 -0600

    everything

---
+++ README.md
@@ -0,0 +1,18 @@
+# codenames-trio
+
+Three player cooperative maps for Codenames
+
+## How it works
+
+Hard coded the base template. Shuffle the rows. Each row represents a card.
+
+## Run locally
+
+```sh
+npm install -g nodemon http-server
+nodemon -x http-server
+```
+
+## Deploy
+
+Push to master
---
+++ js/index.js
@@ -0,0 +1,96 @@
+const board = [[1,   0,   0],
+               [1,   0,   0],
+               [1,  -1,   0],
+               [1,   0,  -1],
+               [1,   0,   1],
+               [1,   1,   0],
+               [-1,  1,   0],
+               [0,   1,  -1],
+               [0,   1,   0],
+               [0,   1,   0],
+               [0,   1,   1],
+               [-1,  0,   1],
+               [0,   0,   1],
+               [0,  -1,   1],
+               [0,   0,   1],
+               [-1,  0,   0],
+               [0,  -1,   0],
+               [0,   0,  -1],
+               [-1, -1,  -1],
+               [0,   0,   0],
+               [0,   0,   0],
+               [0,   0,   0],
+               [0,   0,   0],
+               [0,   0,   0],
+               [0,   0,   0]];
+
+// TODO long term dont use globals, just doing this for ease now
+let gPlayer = -1;
+let gSeed = -1;
+
+const shuffle = (array, seed) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    seed = seed || 1;
+    let random = function() {
+      var x = Math.sin(seed++) * 10000;
+      return x - Math.floor(x);
+    };
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+};
+
+const main = (player, seed) => {
+    const shuffled = shuffle(board.slice(), seed);
+    const gridWrapper = document.querySelector('.wrapper');
+    const cards = gridWrapper.querySelectorAll('.card');
+    cards.forEach((card, i) => {
+        const cardType = shuffled[i][player];
+        if (i == 0) { console.log(i, player, '->', cardType); };
+        let color;
+        switch (cardType) {
+        case undefined:
+            color = 'lightblue';
+            break;
+        case -1:
+            color = 'black';
+            break;
+        case 0:
+            color = 'yellow';
+            break;
+        case 1:
+            color = 'green';
+            break;
+        default:
+            color = 'magenta';
+            break;
+        };
+        card.className = 'card ' + color;
+    });
+};
+
+main(-1, gSeed);
+
+document.querySelectorAll('input[name=player]').forEach((radio) => {
+    radio.addEventListener('change', (e) => {
+        const player = parseInt(e.target.value) - 1;
+        gPlayer = player;
+        main(gPlayer, gSeed);
+    });
+});
+
+const updateSeed = (e) => {
+    const seed = e.target.value;
+    gSeed = parseInt(seed);
+    main(gPlayer, gSeed);
+};
+document.querySelector('#seed').addEventListener('keyup', updateSeed);
+document.querySelector('#seed').addEventListener('change', updateSeed);
