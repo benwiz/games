c2d2d2c7e9a037002b32f80d21a1321734c734dd
benwiz <bwisialowski@gmail.com>
2020-03-20 20:59:06 -0500

    parse json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate websocket;
+extern crate serde_json;
 
 use std::thread;
 use websocket::sync::Server;
 use websocket::OwnedMessage;
+use serde_json::{Value};
 
 fn main() {
     let server = Server::bind("127.0.0.1:2794").unwrap();
@@ -21,7 +23,7 @@ fn main() {
 
             println!("Connection from {}", ip);
 
-            let message = OwnedMessage::Text("Hello".to_string());
+            let message = OwnedMessage::Text("Connected to websocket.".to_string());
             client.send_message(&message).unwrap();
 
             let (mut receiver, mut sender) = client.split().unwrap();
@@ -44,10 +46,13 @@ fn main() {
                         println!("Sent message: {:?}", message);
                         return;
                     }
-                    _ => { // OwnedMessage::Text or OwnedMessage::Binary
-                        print!("Received other. ");
-                        sender.send_message(&message).unwrap();
-                        println!("Sent message: {:?}", message);
+                    OwnedMessage::Text(text) => { // OwnedMessage::Text or OwnedMessage::Binary
+                        let v: Value = serde_json::from_str(&text).unwrap();
+                        println!("v: {}", v);
+                        return;
+                    }
+                    _ => {
+                        println!("Other. Either Pong or Binary.");
                     }
                 }
             }
