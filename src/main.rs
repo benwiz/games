877e2805258c924b0e4915a97d0b85d8ba916b1d
benwiz <bwisialowski@gmail.com>
2020-03-26 21:35:22 -0500

    layout route for room

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,18 @@ extern crate mio_extras;
 extern crate time;
 extern crate ws;
 
-use std::str;
-use std::thread;
-use std::sync::Arc;
-use uuid::Uuid;
 use serde::{Deserialize, Serialize};
-use serde_json::{Value};
-use sled::{Event, IVec, Db};
+use serde_json::Value;
+use sled::{Db, Event, IVec};
+use std::str;
+use std::sync::Arc;
+use std::thread;
+use uuid::Uuid;
 // use mio_extras::timer::Timeout;
 // use ws::util::Token;
 
 // TODO
-// chat struct requires a User
 // rooms route
-
 
 // const PING: Token = Token(1);
 // const EXPIRE: Token = Token(2);
@@ -61,18 +59,16 @@ struct Chats {
 
 fn all_users(db: Arc<Db>) -> Vec<User> {
     let scan = db.scan_prefix("user/");
-    scan.map(
-        |v|
-        {
-            let data = v.unwrap();
-            // let k = IVec::from(data.0);
-            // let key = &str::from_utf8(&k).unwrap();
-            let v = IVec::from(data.1);
-            let value: User = bincode::deserialize(&v).unwrap();
-            // println!("k: {}, v: {}", key, value.name);
-            value
-        }
-    ).collect()
+    scan.map(|v| {
+        let data = v.unwrap();
+        // let k = IVec::from(data.0);
+        // let key = &str::from_utf8(&k).unwrap();
+        let v = IVec::from(data.1);
+        let value: User = bincode::deserialize(&v).unwrap();
+        // println!("k: {}, v: {}", key, value.name);
+        value
+    })
+    .collect()
 }
 
 impl ws::Handler for Server {
@@ -87,20 +83,30 @@ impl ws::Handler for Server {
         let users_msg = Message {
             route: "/users".to_owned(),
             event: "info".to_owned(),
-            body: users_body
+            body: users_body,
         };
         let users_r = serde_json::to_string(&users_msg).unwrap();
         self.out.send(users_r)?;
 
         // Send all chats
-        let chats_encoded: Vec<u8> = self.db.get("chats").unwrap().unwrap_or(IVec::from(vec![])).to_vec(); // NOTE i needed some default. I can probably do this better.
+        let chats_encoded: Vec<u8> = self
+            .db
+            .get("chats")
+            .unwrap()
+            .unwrap_or(IVec::from(vec![]))
+            .to_vec(); // NOTE i needed some default. I can probably do this better.
         let chat_ids: Chats = match chats_encoded.len() {
-            0 => Chats{chats: vec![]},
+            0 => Chats { chats: vec![] },
             _ => bincode::deserialize(&chats_encoded[..]).unwrap(),
         };
         let mut chats: Vec<Chat> = vec![];
         for id in chat_ids.chats {
-            let chat_encoded: Vec<u8> = self.db.get(id.as_bytes()).unwrap().unwrap_or(IVec::from(vec![])).to_vec();
+            let chat_encoded: Vec<u8> = self
+                .db
+                .get(id.as_bytes())
+                .unwrap()
+                .unwrap_or(IVec::from(vec![]))
+                .to_vec();
             if chat_encoded.len() > 0 {
                 let chat: Chat = bincode::deserialize(&chat_encoded[..]).unwrap();
                 chats.push(chat);
@@ -118,7 +124,7 @@ impl ws::Handler for Server {
         // Initialize users subscriber
         let db_users = self.db.clone();
         let out_users = self.out.clone();
-        thread::spawn(move ||  -> ws::Result<()> {
+        thread::spawn(move || -> ws::Result<()> {
             let events = db_users.watch_prefix("user/");
             for event in events {
                 let msg = match event {
@@ -129,11 +135,13 @@ impl ws::Handler for Server {
                             event: "create".to_owned(),
                             body: serde_json::to_value(user).unwrap(),
                         }
-                    },
+                    }
                     Event::Remove(k) => {
                         let key = str::from_utf8(&k).unwrap().to_string();
                         let split: Vec<&str> = key.split("/").collect();
-                        let id = Id { id: split[1].to_owned() };
+                        let id = Id {
+                            id: split[1].to_owned(),
+                        };
                         Message {
                             route: "/users".to_owned(),
                             event: "delete".to_owned(),
@@ -151,7 +159,7 @@ impl ws::Handler for Server {
         // Subscribe to chat events
         let db_chat = self.db.clone();
         let out_chat = self.out.clone();
-        thread::spawn(move ||  -> ws::Result<()> {
+        thread::spawn(move || -> ws::Result<()> {
             let events = db_chat.watch_prefix("chat/");
             for event in events {
                 match event {
@@ -166,7 +174,7 @@ impl ws::Handler for Server {
                         };
                         let r = serde_json::to_string(&msg).unwrap();
                         out_chat.send(r)?
-                    },
+                    }
                     Event::Remove(_k) => {} // No delete
                 };
             }
@@ -194,23 +202,35 @@ impl ws::Handler for Server {
         //     }
         // };
         match m.route.as_str() {
-            "/echo" => {
-                self.out.send(msg)
-            },
+            "/echo" => self.out.send(msg),
             "/users" => {
                 m.body["id"] = Value::String(self.id.to_hyphenated().to_string());
                 let user: User = serde_json::from_value(m.body).unwrap();
                 let k = format!("user/{}", self.id.to_hyphenated());
                 let v = bincode::serialize(&user).unwrap();
                 match self.db.insert(&k, v) {
-                    Ok(_t) => {},
+                    Ok(_t) => {}
                     Err(_e) => {
                         // TODO do something
+                        println!("Silently failing to insert user.")
                     }
                 }
 
                 Ok(())
-            },
+            }
+            "/rooms" => {
+                match m.event.as_str() {
+                    "create" => {
+                        println!("create room");
+                    }
+                    "join" => {
+                        println!("join room");
+                    }
+                    _ => {}
+                }
+
+                Ok(())
+            }
             "/chat" => {
                 let user_k = format!("user/{}", self.id.to_hyphenated());
                 println!("user_k {}", user_k);
@@ -225,7 +245,7 @@ impl ws::Handler for Server {
                     let k = format!("chat/{}", Uuid::new_v4().to_hyphenated());
                     let v = bincode::serialize(&chat).unwrap();
                     match self.db.insert(&k.as_bytes(), v) {
-                        Ok(_t) => {},
+                        Ok(_t) => {}
                         Err(_e) => {
                             // TODO do something
                             println!("Silently failing when tring to insert chat.");
@@ -234,9 +254,14 @@ impl ws::Handler for Server {
 
                     // Update chats list
                     let chats_k = "chats";
-                    let chats_encoded: Vec<u8> = self.db.get(chats_k).unwrap().unwrap_or(IVec::from(vec![])).to_vec(); // NOTE I needed some default. I can probably do this better.
+                    let chats_encoded: Vec<u8> = self
+                        .db
+                        .get(chats_k)
+                        .unwrap()
+                        .unwrap_or(IVec::from(vec![]))
+                        .to_vec(); // NOTE I needed some default. I can probably do this better.
                     let mut chats: Chats = match chats_encoded.len() {
-                        0 => Chats{chats: vec![]},
+                        0 => Chats { chats: vec![] },
                         _ => bincode::deserialize(&chats_encoded[..]).unwrap(), // TODO I think chats_encoded.as_str() would also work
                     };
                     chats.chats.insert(0, k.clone());
@@ -247,7 +272,7 @@ impl ws::Handler for Server {
                     };
                     let chats_v: Vec<u8> = bincode::serialize(&chats).unwrap();
                     match self.db.insert(&chats_k, chats_v) {
-                        Ok(_t) => {},
+                        Ok(_t) => {}
                         Err(_e) => {
                             // TODO do something
                             println!("Silently failing when trying to inserts chats list.");
@@ -257,7 +282,7 @@ impl ws::Handler for Server {
                     // Remove chat records no longer in chat list
                     for chat_id in remove_ids {
                         match self.db.remove(&chat_id) {
-                            Ok(_t) => {},
+                            Ok(_t) => {}
                             Err(_e) => {
                                 // TODO do something
                                 println!("Silently failing when trying to remove chat.");
@@ -266,7 +291,7 @@ impl ws::Handler for Server {
                     }
                 }
                 Ok(())
-            },
+            }
             _ => {
                 println!("Unknown route: {}", m.route);
                 Ok(())
@@ -288,9 +313,10 @@ impl ws::Handler for Server {
         let user = format!("user/{}", self.id);
         println!("Removing {}", user);
         match self.db.remove(user) {
-            Ok(_t) => {},
+            Ok(_t) => {}
             Err(_e) => {
                 // TODO do something
+                println!("Silently failing removing user on close.");
             }
         }
 
@@ -298,8 +324,10 @@ impl ws::Handler for Server {
         // Once i do I should be able to return this line, and the db remove
         // can use a `?`.
         match self.out.close(code) {
-            Ok(_t) => {},
-            Err(_e) => {},
+            Ok(_t) => {}
+            Err(_e) => {
+                println!("Silently failing to close connection.");
+            }
         }
     }
 
@@ -361,7 +389,12 @@ impl ws::Handler for Server {
 
 fn main() {
     // let db = Arc::new(sled::open("game_db").expect("Sled must start ok."));
-    let db = Arc::new(sled::Config::new().temporary(true).open().expect("Sled must start ok."));
+    let db = Arc::new(
+        sled::Config::new()
+            .temporary(true)
+            .open()
+            .expect("Sled must start ok."),
+    );
 
     ws::listen("0.0.0.0:3012", |out| {
         Server {
@@ -371,5 +404,6 @@ fn main() {
             // ping_timeout: None,
             // expire_timeout: None,
         }
-    }).unwrap()
+    })
+    .unwrap()
 }
