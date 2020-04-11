5ef10a1939870e8542409ee1533029b3567b6478
benwiz <bwisialowski@gmail.com>
2020-04-11 12:18:22 -0700

    todo

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ use uuid::Uuid;
 
 // TODO
 // - identify winner
-// - delete user
-// - there will definitely be some bugs related to users leaving rooms. Maybe they shouldn't be removed? Or the game should auto reset?
+// - finish sending error messages to client
+// - NOTE: there will definitely be some bugs related to users leaving rooms. Maybe they shouldn't be removed? Or the game should auto reset?
 // - OPTIMIZATION: Because all clients are getting updates on all rooms and all games EITHER separate out game resource so client can subscribe to game of interest OR do filtering at the client level to not update if the changes are in the game sub-struct. Idk if this is easily doable.
 // - OPTIMIZATION: Using the recovery feature it is possible to hijack someone's "account". In order to prevent this I would need to provide a secret ID on open.
 
