52c976d693f63ff6fffe41de33a6349d150d47a9
benwiz <bwisialowski@gmail.com>
2020-04-12 22:32:28 -0700

    new release

--- release.sh
+++ release.sh
@@ -5,5 +5,5 @@ filename=$(jq '.[0]["output-name"]' docs/js/compiled/manifest.json)
 filename="${filename%\"}" # strip first quote
 filename="${filename#\"}" # strip last quote
 echo "new filename is: $filename"
-sed -i .bak "s/main\..*\.js/$filename/" docs/index.html
+sed -i "s/main\..*\.js/$filename/" docs/index.html
 cat docs/index.html
