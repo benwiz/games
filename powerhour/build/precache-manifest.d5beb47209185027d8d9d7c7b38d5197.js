a8deafbeec5a2e9751b45c64521bd5759fde25f9
benwiz <bwisialowski@gmail.com>
2020-05-05 18:49:16 -0700

    build powerhour

---
+++ powerhour/build/precache-manifest.d5beb47209185027d8d9d7c7b38d5197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6ca1583afbb63c078cb",
+    "url": "/powerhour/static/css/main.a879bcae.chunk.css"
+  },
+  {
+    "revision": "b6ca1583afbb63c078cb",
+    "url": "/powerhour/static/js/main.b6ca1583.chunk.js"
+  },
+  {
+    "revision": "e722ba36f86c06c4d63c",
+    "url": "/powerhour/static/js/runtime~main.e722ba36.js"
+  },
+  {
+    "revision": "5146c9762e47d6e310b8",
+    "url": "/powerhour/static/js/2.5146c976.chunk.js"
+  },
+  {
+    "revision": "f4fce171b254da3dd117b37bbe81e14d",
+    "url": "/powerhour/index.html"
+  }
+];
