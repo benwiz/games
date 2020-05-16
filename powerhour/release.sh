e1784b68a078d3c5ffb8821060569fc8374a66b5
benwiz <bwisialowski@gmail.com>
2020-05-16 11:17:46 -0700

    mess around with shadow

--- powerhour/release.sh
+++ powerhour/release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 yarn release
-filename=$(jq '.[0]["output-name"]' js/compiled/manifest.json)
+filename=$(jq '.[0]["output-name"]' docs/js/compiled/manifest.json)
 filename="${filename%\"}" # strip first quote
 filename="${filename#\"}" # strip last quote
 echo "new filename is: $filename"
-sed -i "s/main\..*\.js/$filename/" index.html # for mac inset `.bak` after the `-i`
-cat index.html
-git add "js/compiled/$filename"
-git add "index.html"
+sed -i "s/main\..*\.js/$filename/" docs/index.html # for mac inset `.bak` after the `-i`
+cat docs/index.html
+git add "docs/js/compiled/$filename"
+git add "docs/index.html"
 git commit -m "release $filename"
