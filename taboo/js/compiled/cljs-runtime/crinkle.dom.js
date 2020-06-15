ca05ab05c5b705021e93e2a0d2e415c696dcd42f
Ben Wiz <bwisialowski@gmail.com>
2020-06-14 17:08:52 -0700

    clean up compiled

--- taboo/js/compiled/cljs-runtime/crinkle.dom.js
+++
@@ -1,6 +0,0 @@
-goog.provide('crinkle.dom');
-goog.require('cljs.core');
-var module$node_modules$react$index=shadow.js.require("module$node_modules$react$index", {});
-goog.require('crinkle.component');
-
-//# sourceMappingURL=crinkle.dom.js.map
